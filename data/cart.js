6a3bd95cd1f211e443c64fb21b298cad7c68405f
peter-perger <pergerpeti2@gmail.com>
2025-05-05 13:13:33 +0200

    Created saveCart function and saved the cart to localStorage

--- data/cart.js
+++ data/cart.js
@@ -1,11 +1,11 @@
-let cart = [{
-    productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-    quantity: 1
-    },{
-    productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
-    quantity: 2
-    }
-]
+let cart = JSON.parse(localStorage.getItem('cart')) || [{
+        productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+        quantity: 1
+        },{
+        productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+        quantity: 2
+        }
+    ]
 
 export function calculateCartQuantity() {
     let quantity = 0;
@@ -17,7 +17,7 @@ export function calculateCartQuantity() {
     return quantity;
 }
 
-function getMatchingItem (productId) {
+export function getMatchingItem (productId) {
     let matchingItem;
     
     cart.forEach((cartItem) => {
@@ -46,6 +46,10 @@ export function addToCart (productId) {
         })
     }
 
+    saveCart();
     console.log(cart);
 }
 
+export function saveCart() {
+    localStorage.setItem('cart', JSON.stringify(cart));
+}
